4512ef554f79679e3bc8d58f4acbca70da76bf68
Hugo Fonseca <hugo.fonseca@jumia.com>
2018-02-21 18:59:33 +0000

    Fixed pipeline

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,11 +1,11 @@
 node {
-    lib_release = load "./lit_test.groovy"
-
     stage('Checkout') {
         timeout(time: 10, unit:'MINUTES'){
             checkout scm
         }
     }
+
+    lib_release = load "./lib_test.groovy"
 
     stage('tag') {
         //withCredentials([usernamePassword(credentialsId: 'jumia-integrations-token', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
@@ -17,9 +17,9 @@ node {
         //        curl --max-time 10 --silent --user "${GIT_USERNAME}:${GIT_PASSWORD}" -H "Content-Type: application/json" -X POST -d '{"tag_name": "'${tag_name}'", "target_commitish": "'${commit_id}'"}' "https://api.github.com/repos/marcelosousaalmeida/test-rc/releases"
         //    ''')
         //}
-        def tagName = env.BRANCH_NAME + '-' + env.BUILD_ID
-        def commitID = sh(returnStdout: true, script: 'git log -1 --pretty=format:"%H"').trim()
-        status = lib_release.createRelease(tagName, commitID)
+        String tagName = env.BRANCH_NAME + '-' + env.BUILD_ID
+        String commitID = sh(returnStdout: true, script: 'git log -1 --pretty=format:"%H"').trim()
+        status = lib_release.createRelease(tagName, commitID,"test-rc", "marcelosousaalmeida")
     }
 }
 
--- lib_test.groovy
+++ lib_test.groovy
@@ -1,33 +1,26 @@
-def createRelease(tagName, commitID, name="", body="", draft=false, prerelease=false) {
-    String owner = "marcelosousaalmeida"
-    String repo = "test-rc"
-
+def createRelease(tagName, commitID, repo, owner="JumiaAIG", name="", body="", draft=false, prerelease=false) {
     if (name == "") {
         name = tagName
     }
+
     withCredentials([usernamePassword(credentialsId: 'jumia-integrations-token', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
         String callAPI = """
-        set -ex;
-        curl -s -m 10 -XPOST \
-            --user ${GIT_USERNAME}:{GIT_PASSWORD}
-            --url https://api.github.com/repos/${owner}/${repo}/releases
-            --header "Content-Type: application/json"
-            --data \'{
+        set +x && \
+        curl -f -s -m 10 -X POST \
+            --user ${GIT_USERNAME}:${GIT_PASSWORD} \
+            --url https://api.github.com/repos/${owner}/${repo}/releases \
+            --header "Content-Type: application/json" \
+            --data '{
                 "tag_name": "${tagName}",
-                "target_commitish": "${commitID},
-                "name": "${name}",
-                "body": "${body}",
+                "target_commitish": "${commitID}",
+                "name": "${name}","body": "${body}",
                 "draft": ${draft},
                 "prerelease": ${prerelease}
-            "}\' > /tmp/${tagName}_${commitID}.txt
+            }'
         """
-
-        print(callAPI)
 
         status = sh(returnStatus: true, script: callAPI)
     }
-
-    print(status)
 
     return status
 }
